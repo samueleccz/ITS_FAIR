b8c41239517d18cc3db7a930682d8418f26b789d
samueleccz <s.cucuzza@outlook.com>
2023-04-01 19:14:23 +0200

    Ho sistemato il codice

--- C++/prova.cpp
+++ C++/prova.cpp
@@ -6,10 +6,13 @@ using namespace std;
 
 class prova{
     private:
-        int a;
+        int lato1;
         int b;
+        int somma;
     public:
-        prova(int x, int y) : a(x) , b(y){}
+        prova(int x, int y) : lato1(x) , b(y){
+            somma = lato1+b;
+        }
 };
 
 
