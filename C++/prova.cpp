4d72446a8efd4f0aa89209f44127d4f90c5c525c
samueleccz <s.cucuzza@outlook.com>
2023-04-01 19:11:20 +0200

    Ho aggiunto la classe Prova

--- C++/prova.cpp
+++ C++/prova.cpp
@@ -4,6 +4,15 @@
 using namespace std;
 
 
+class prova{
+    private:
+        int a;
+        int b;
+    public:
+        prova(int x, int y) : a(x) , b(y){}
+};
+
+
 int main(){
 
     cout << "ciao";
